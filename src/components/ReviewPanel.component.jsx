8d56a90aa32b1a78284763a1f94cd39c5ed152f4
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2021-04-29 10:26:53 +0200

    Make refactoring and move the review content in a personal component
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- src/components/ReviewPanel.component.jsx
+++ src/components/ReviewPanel.component.jsx
@@ -1,66 +1,58 @@
 import React from "react"
-import { Grid, useMediaQuery } from "@material-ui/core"
 import ReviewCard from "./ReviewCard.component"
 import Carousel from "react-multi-carousel";
 
-function ReviewPanelMediaQuery({ items }) {
-  let totalItems = 12; //matches === true ? 4 : 12
-  //let spacing = totalItems < 12 ? 4 : 0
-
-  let component = undefined;
-
-  return (
-      <Carousel
-        s
-        transitionDuration={500}
-        autoPlaySpeed={10}
-        responsive={{
-          desktop: {
-            breakpoint: {
-              max: 3000,
-              min: 1024
-            },
-            items: 3,
-            partialVisibilityGutter: 40
-          },
-          mobile: {
-            breakpoint: {
-              max: 464,
-              min: 0
-            },
-            items: 1,
-            partialVisibilityGutter: 30
-          },
-          tablet: {
-            breakpoint: {
-              max: 1024,
-              min: 464
-            },
-            items: 2,
-            partialVisibilityGutter: 30
-          }
-        }}
-      >
-        {items.map((item, index) =>
-          <Grid item xs={totalItems} key={index}>
-            <ReviewCard
-              authorName={item.author_name}
-              authorRate={item.rating}
-              authorImage={item.profile_photo_url}
-              authorText={item.text}
-            />
-          </Grid>
-        )}
-      </Carousel>
-  )
-}
-
 class ReviewPanel extends React.Component {
+  constructor(props, context) {
+    super(props, context)
+    this.state = {
+      responsive: {
+        superLargeDesktop: {
+          // the naming can be any, depends on you.
+          breakpoint: { max: 4000, min: 3000 },
+          items: 5
+        },
+        desktop: {
+          breakpoint: { max: 3000, min: 1024 },
+          items: 3
+        },
+        tablet: {
+          breakpoint: { max: 1024, min: 464 },
+          items: 1
+        },
+        mobile: {
+          breakpoint: { max: 464, min: 0 },
+          items: 1
+        }
+      }
+    }
+  }
 
   render() {
     let { items } = this.props
     return (
-        <ReviewPanelMediaQuery items={items} />
+      <Carousel
+        wipeable={true}
+        draggable={false}
+        showDots={true}
+        infinite={true}
+        autoPlay={true}
+        autoPlaySpeed={5000}
+        keyBoardControl={true}
+        responsive={this.state.responsive}
+      >
+        {items.map((item, index) =>
+          <ReviewCard
+            keyVal={index}
+            authorName={item.author_name}
+            authorRate={item.rating}
+            authorImage={item.profile_photo_url}
+            authorText={item.text}
+            dateReview={item.relative_time_description}
+            link={item.author_url}
+          />
+        )}
+      </Carousel>
     )
   }
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,12 @@
 import React from "react"
-import { MuiThemeProvider, CssBaseline, Grid, Avatar, useMediaQuery } from "@material-ui/core"
+import { MuiThemeProvider, CssBaseline, Grid, Avatar } from "@material-ui/core"
 import theme from "../themes/dark"
 import BasicAppBar from "../components/BasicAppBar.component"
 import { graphql, StaticQuery } from "gatsby"
-import ReviewCard from "../components/ReviewCard.component"
-import Carousel from "react-multi-carousel"
 
 // import style
 import "react-multi-carousel/lib/styles.css"
+import ReviewPanel from "../components/ReviewPanel.component"
 
 export default class Home extends React.Component {
   render() {
@@ -21,31 +20,6 @@ export default class Home extends React.Component {
 }
 
 class Index extends React.Component {
-  constructor(props, context) {
-    super(props, context)
-    this.state = {
-      responsive: {
-        superLargeDesktop: {
-          // the naming can be any, depends on you.
-          breakpoint: { max: 4000, min: 3000 },
-          items: 5
-        },
-        desktop: {
-          breakpoint: { max: 3000, min: 1024 },
-          items: 3
-        },
-        tablet: {
-          breakpoint: { max: 1024, min: 464 },
-          items: 1
-        },
-        mobile: {
-          breakpoint: { max: 464, min: 0 },
-          items: 1
-        }
-      }
-    }
-  }
-
   render() {
     return (
       <StaticQuery
@@ -84,49 +58,10 @@ class Index extends React.Component {
                       }}
               />
             </Grid>
-            <Carousel
-              wipeable={true}
-              draggable={false}
-              showDots={true}
-              infinite={true}
-              autoPlay={true}
-              autoPlaySpeed={5000}
-              keyBoardControl={true}
-              responsive={this.state.responsive}
-            >
-              {data.googlePlacesPlace.childrenGooglePlacesReview.map((item, index) =>
-                <ReviewCard
-                  keyVal={index}
-                  authorName={item.author_name}
-                  authorRate={item.rating}
-                  authorImage={item.profile_photo_url}
-                  authorText={item.text}
-                  dateReview={item.relative_time_description}
-                  link={item.author_url}
-                />
-              )}
-            </Carousel>
+            <ReviewPanel items={data.googlePlacesPlace.childrenGooglePlacesReview} />
           </>
         )}
       />
     )
   }
 }
-
-/**
- *
- */
-
-/**
- *  {
-                data.googlePlacesPlace.childrenGooglePlacesReview.map((author, index) => (
-                  <ReviewCard
-                    keyVal={index}
-                    authorName={author.author_name}
-                    authorRate={author.rating}
-                    authorImage={author.profile_photo_url}
-                    authorText={author.text}
-                  />
-                ))
-              }
- */
